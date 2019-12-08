374a72c1364d68506feb4795c9a4a51fd6be0e38
gershkor@wincs.cs.bgu.ac.il <ronengershko12@gmail.com>
2019-12-08 21:20:46 +0200

    Merge branch 'master' of /users/studs/bsc/2020/gershkor/Desktop/SPLMI6 with conflicts.

--- src/main/java/bgu/spl/mics/Event.java
+++ src/main/java/bgu/spl/mics/Event.java
@@ -1,5 +1,5 @@
 package bgu.spl.mics;
-// Kill me
+
 /**
  * A "Marker" interface extending {@link Message}. A Publisher that sends an
  * Event message expects to receive a result of type {@code <T>} when a
