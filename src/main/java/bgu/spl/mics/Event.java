35cbb7cf2d57820ccf1b5e725ae0a09a263705d2
gershkor@wincs.cs.bgu.ac.il <ronengershko12@gmail.com>
2019-12-08 20:59:26 +0200

    ata homo

--- src/main/java/bgu/spl/mics/Event.java
+++ src/main/java/bgu/spl/mics/Event.java
@@ -1,5 +1,5 @@
 package bgu.spl.mics;
-//i like big buts
+//magic stick
 /**
  * A "Marker" interface extending {@link Message}. A Publisher that sends an
  * Event message expects to receive a result of type {@code <T>} when a
