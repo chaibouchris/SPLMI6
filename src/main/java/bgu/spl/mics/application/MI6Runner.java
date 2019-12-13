243f62ebe10f5b502201491a675ab3f97c021883
chaibouchris <chai.bouchris333@gmail.com>
2019-12-13 18:11:41 +0200

    fuck you shokti

--- src/main/java/bgu/spl/mics/application/MI6Runner.java
+++ src/main/java/bgu/spl/mics/application/MI6Runner.java
@@ -1,6 +1,15 @@
 package bgu.spl.mics.application;
 
-/** This is the Main class of the application. You should parse the input file, 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
+/** This is the Main class of the application. You should parse the input file,
  * create the different instances of the objects, and run the system.
  * In the end, you should output serialized objects.
  */
@@ -8,4 +17,17 @@ public class MI6Runner {
     public static void main(String[] args) {
         // TODO Implement this
     }
+
+    public static JsonObject Read (String file) throws FileNotFoundException {
+        Gson park = new GsonBuilder().setPrettyPrinting().create();
+        JsonObject Derulo = null;
+        try (Reader reader = new FileReader(file)){
+            JsonElement Kid = park.fromJson(reader,JsonElement.class);
+            Derulo = Kid.getAsJsonObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Derulo;
+    }
 }
+
--- src/test/java/bgu/spl/mics/InventoryTest.java
+++ src/test/java/bgu/spl/mics/InventoryTest.java
@@ -1,20 +1,59 @@
 package bgu.spl.mics;
 
+import bgu.spl.mics.application.MI6Runner;
+import bgu.spl.mics.application.passiveObjects.Inventory;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InventoryTest {
+
+    private Inventory Invi;
+    private String[] Gadgets = {"SatSolver","Pizza","Burekas","Tinder","Lupital"};
+
     @BeforeEach
     public void setUp(){
-
-
+        try {
+            Invi = Inventory.getInstance();
+            Invi.load(Gadgets);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void test(){
-        //TODO: change this test and add more tests :)
-        fail("Not a good test");
+    public void GetItemsTest(){
+        assertTrue(Invi.getItem("SatSolver"));
+        assertFalse(Invi.getItem("SatSolver"));
+        assertFalse(Invi.getItem("Black Zucchini"));
+    }
+
+    @Test
+    public void PrintToFileTest() {
+        try {
+            Invi.printToFile("inventoryOutputFile.json");
+            JsonObject read = MI6Runner.Read("inventoryOutputFile.json");
+            JsonArray Harry = read.getAsJsonArray("inventory");
+            List<String> invi = new ArrayList<>();
+            for (int i = 0; i < Harry.size(); i++) {
+                invi.add(Harry.get(i).toString());
+            }
+            assertEquals(5,Harry.size());
+            while (!invi.isEmpty()){
+                assertTrue(Invi.getItem(invi.get(0)));
+                invi.remove(0);
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
