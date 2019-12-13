9497b439571542cf43ac9a85892e6672d2ea0323
chaibouchris <chai.bouchris333@gmail.com>
2019-12-13 15:54:39 +0200

    fuck you shokti

--- src/test/java/bgu/spl/mics/SquadTest.java
+++ src/test/java/bgu/spl/mics/SquadTest.java
@@ -1,14 +1,25 @@
 package bgu.spl.mics;
 
+import bgu.spl.mics.application.passiveObjects.Agent;
+import bgu.spl.mics.application.passiveObjects.Squad;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class SquadTest {
+
+    private Squad squad;
+    private Agent[] agents;
+
     @BeforeEach
     public void setUp(){
-
+        try {
+            squad = Squad.getInstance();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     @Test
