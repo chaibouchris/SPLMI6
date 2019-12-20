26c18f2e20cd82c0847da1bf7fe77e2b0db677a3
chai333 <chai.bouchris333@gmail.com>
2019-12-20 16:05:43 +0200

    im the bird of freedom!

--- src/main/java/bgu/spl/mics/application/messages/TerminateBroadcast.java
+++ src/main/java/bgu/spl/mics/application/messages/TerminateBroadcast.java
@@ -1,8 +1,6 @@
 package bgu.spl.mics.application.messages;
 
 import bgu.spl.mics.Broadcast;
-import bgu.spl.mics.Event;
 
 public class TerminateBroadcast implements Broadcast {
-    int terminate = 0;
 }
