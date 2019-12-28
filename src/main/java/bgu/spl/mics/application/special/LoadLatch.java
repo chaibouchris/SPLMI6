47ab299e8b374dceaf92988486bb101c14f445c6
chai333 <chai.bouchris333@gmail.com>
2019-12-28 18:58:54 +0200

    im the bird of freedom!

--- src/main/java/bgu/spl/mics/application/special/LoadLatch.java
+++ src/main/java/bgu/spl/mics/application/special/LoadLatch.java
@@ -3,24 +3,37 @@ package bgu.spl.mics.application.special;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * this class was made for make sure that we start the TimeService after all the other threads
+ * are ready to start the missions.
+ * the fields contains list of threads that include all the subscribers, the thread of the TimeService
+ * and a countdownlatch that we use for that.
+ */
 public class LoadLatch {
     private List<Thread> threadList;
     private Thread timeService;
     private static CountDownLatch latchi;
 
+    /**
+     * public constructor of this class.
+     */
     public LoadLatch(List<Thread> threadList, Thread timeService){
         this.threadList = threadList;
         this.timeService = timeService;
         latchi = new CountDownLatch(threadList.size());
     }
 
+    /**
+     * a method that we use for start all the subscribers threads and wait until everyone start together and
+     * than TimeService start
+     */
     public void start(){
         for (Thread thread: threadList){
             thread.start();
             System.out.println(thread.getName()+" start");
         }
         try {
-            latchi.await();
+            latchi.await();//wait until countdown get to 0
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -29,6 +42,9 @@ public class LoadLatch {
         System.out.println("timeService start");
     }
 
+    /**
+     * make a countdown in the latch
+     */
     public static void CountdownLatch(){
         latchi.countDown();
     }
